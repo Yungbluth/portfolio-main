92584f42c134908ae7eb6909ba4583e3fc9aae96
Yungbluth <46689359+Yungbluth@users.noreply.github.com>
2024-10-09 18:02:06 -0700

    Fix Typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="intro">
       <h2 className="intro-tagline">
         I'm <span className="name">Matthew Yungbluth</span>, a software engineer based
-        in Tucson, Arizona. I love love spending time coding and encountering new challenges. I'm highly adaptable to new challenges and projects.
+        in Tucson, Arizona. I love spending time coding and encountering new challenges. I'm highly adaptable to new challenges and projects.
       </h2>
       <h3 className="intro-contact">
         <span>Get in touch 👉🏼</span>{" "}
