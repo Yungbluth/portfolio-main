88f9ebef19076e3d8b49fd10a17e4949c2c831f6
Yungbluth <46689359+Yungbluth@users.noreply.github.com>
2025-04-02 01:40:20 -0700

    More accurate and added buttons
    Added an additional hidden layer in the neural network. Also changed clear canvas and predict from mouse buttons to actual buttons.

--- src/components/DisplayProjects/Numbers.js
+++ src/components/DisplayProjects/Numbers.js
@@ -8,19 +8,23 @@ function Numbers() {
 
   let isDrawing = false;
   let heightmax = document.documentElement.clientHeight * 0.8 - 50;
-  let widthmax = document.documentElement.clientWidth * 0.8
+  let widthmax = document.documentElement.clientWidth * 0.8 - 200;
 
+  //Grab all the weights and biases data from file
   let weightOne;
   let biasOne;
   let weightTwo;
   let biasTwo;
+  let weightThree;
+  let biasThree;
   fetch(weightsBiases).then((res) => res.text()).then((text) => {
     let totalData = text.split("l");
      weightOne = totalData[0].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);});
-     console.log(weightOne[7]);
      biasOne = totalData[1].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);})[0];
      weightTwo = totalData[2].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);});
      biasTwo = totalData[3].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);})[0];
+     weightThree = totalData[4].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);});
+     biasThree = totalData[5].split("e").map(function(e){return e.match(/-?\d+(?:\.\d+)?/g).map(Number);})[0];
   }).catch((e) => console.error(e));
 
 
@@ -35,7 +39,7 @@ function Numbers() {
     const context = canvas.getContext('2d');
     //draws pixels nicer
     context.imageSmoothingEnabled = false;
-    context.translate(-.5,-.5);
+    //context.translate(-.5,-.5);
     context.lineWidth = 2;
     context.lineCap = "round";
 
@@ -49,14 +53,14 @@ function Numbers() {
       //math to connect the large canvas with the 28x28 pixel grid
       let bounds = canvas.getBoundingClientRect();
       let percentage = 1/28;
-      //context.lineTo(Math.ceil(((e.clientX - bounds.x) / bounds.width)/percentage), Math.ceil(((e.clientY - bounds.y) / bounds.height)/percentage));
-      //context.stroke();
-      
+      context.lineTo(Math.ceil(((e.clientX - bounds.x) / bounds.width)/percentage), Math.ceil(((e.clientY - bounds.y) / bounds.height)/percentage));
+      context.stroke();
+      /*
       let xpos = ((e.clientX - bounds.x) / bounds.width)/percentage;
       let ypos = ((e.clientY - bounds.y) / bounds.height)/percentage;
       let pxData = context.getImageData(xpos,ypos,1,1);
       pxData.data[3]=255;
-      context.putImageData(pxData,xpos,ypos);
+      context.putImageData(pxData,xpos,ypos);*/
     };
     
     //Start drawing
@@ -65,45 +69,13 @@ function Numbers() {
       if (e.button === 0) {
         isDrawing = true;
       }
-      if (e.button === 1) {
-        //middle mouse send to NN
-        let curPixels = context.getImageData(0,0,canvas.height,canvas.width);
-        let pixelDataArray = curPixels.data;
-        let drawnImageArray = [];
-        for (let i = 0; i < pixelDataArray.length; i+=4) {
-          /*
-          if (pixelDataArray[i+3] === 255) {
-            drawnImageArray.push(255);
-          } else {
-            drawnImageArray.push(0);
-          }*/
-         drawnImageArray.push(pixelDataArray[i+3]);
-        }
-        //Get the pretrained weights and biases from txt file and do some string magic to put them into arrays
-        
-
-        let input = multiply(drawnImageArray, weightOne);
-        let hidden = relu(input.map((x,y) => x + biasOne[y]));
-        let scores = multiply(hidden, weightTwo).map((x,y) => x + biasTwo[y]);
-        let probs = softmax(scores);
-        let predict = probs.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
-        console.log(predict);
-          
-
-        //input_layer = np.dot(x_test[num:num+1], weight1)
-        //input layer is image pixels dot weights1
-        //hidden layer is relu(input layer + bias1)
-        //scores is hidden layer dot weights2 + bias2
-        //probs is softmax(scores)
-        //predict is argmax(probs)
-      }
     });
 
     //Stop drawing
     canvas.addEventListener('mouseup', (e) => {
       isDrawing = false;
-      //context.stroke();
-      //context.beginPath();
+      context.stroke();
+      context.beginPath();
     });
 
     //Middle of drawing
@@ -111,12 +83,6 @@ function Numbers() {
 
     //Allows for dots
     canvas.addEventListener('mousedown', draw);
-
-    //Clears the canvas, currently on right-click
-    //TODO: Change to a button
-    canvas.addEventListener('contextmenu', (e) => {
-      context.clearRect(0, 0, widthmax, heightmax);
-    });
     
     // Clean up function
     return () => {
@@ -125,10 +91,12 @@ function Numbers() {
 
   }, []);
 
+  //Turn all negative numbers to 0
   function relu(arrInput) {
     return arrInput.map((x) => Math.max(x, 0));
   }
 
+  //turn into probabilities
   function softmax(arr) {
     const C = Math.max(...arr);
     const d = arr.map((y) => Math.exp(y - C)).reduce((a, b) => a + b);
@@ -137,9 +105,54 @@ function Numbers() {
     })
   }
 
+  function clearCanvas() {
+    let context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, widthmax, heightmax);
+  }
+
+  //Takes current drawing on canvas and runs it through the neural network
+  function predictNumber() {
+      let canvas = canvasRef.current;
+      let context = canvas.getContext('2d');
+      let curPixels = context.getImageData(0,0,canvas.height,canvas.width);
+      let pixelDataArray = curPixels.data;
+      let drawnImageArray = [];
+      for (let i = 0; i < pixelDataArray.length; i+=4) {
+        /*
+        if (pixelDataArray[i+3] === 255) {
+          drawnImageArray.push(255);
+        } else {
+          drawnImageArray.push(0);
+        }*/
+       drawnImageArray.push(pixelDataArray[i+3]);
+      }
+      //Get the pretrained weights and biases from txt file and do some string magic to put them into arrays
+      
+
+      let input = multiply(drawnImageArray, weightOne);
+      let hidden = relu(input.map((x,y) => x + biasOne[y]));
+      let hiddenDot = multiply(hidden, weightTwo);
+      let hiddenTwo = relu(hiddenDot.map((x,y) => x + biasTwo[y]));
+      let scores = multiply(hiddenTwo, weightThree).map((x,y) => x + biasThree[y]);
+      let probs = softmax(scores);
+      let predict = probs.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+      document.getElementById("predictedNum").innerHTML = predict;
+      console.log(predict);
+  }
+
 
   return (
+    <div>
+      <div style={{float: "left"}}>
     <canvas ref={canvasRef} id="canvas"/>
+    </div>
+    <button onClick={clearCanvas}>Clear Canvas</button>
+    <button onClick={predictNumber}>Predict</button>
+    <div>
+      Your number is:
+      <div id="predictedNum"></div>
+    </div>
+    </div>
   );
 }
 
